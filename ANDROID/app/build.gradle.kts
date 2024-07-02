71f46eb884cd604d572864eb37ddee8d125be1a8
thanhhoa2002 <thanhhoa191202@gmail.com>
2024-07-02 13:42:08 +0700

    done lagging fix

--- ANDROID/app/build.gradle.kts
+++ ANDROID/app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 }
--- ANDROID/app/src/main/java/com/example/mhqltt/ActivityImageInFile.java
+++ ANDROID/app/src/main/java/com/example/mhqltt/ActivityImageInFile.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.LruCache;
 import android.view.View;
 import android.widget.Button;
 
@@ -22,12 +24,13 @@ public class ActivityImageInFile extends AppCompatActivity {
     private FileManager fileManager;
     private RecyclerView recyclerView;
     private ImageAdapter imageAdapter;
-    private List<Bitmap> allImages;
     private List<Bitmap> displayedImages;
     private Bitmap selectedImage;
     private Button showImageButton, previousButton, nextButton;
     private int currentPage = 0;
-    private static final int IMAGES_PER_PAGE = 18;
+    private static final int IMAGES_PER_PAGE = 15;
+    private List<DirectoryEntry> directoryEntries = null;
+    private LruCache<String, Bitmap> bitmapCache;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +45,22 @@ public class ActivityImageInFile extends AppCompatActivity {
 
         recyclerView.setLayoutManager(new GridLayoutManager(this, 3)); // Set GridLayoutManager with 3 columns
 
-        allImages = new ArrayList<>();
         displayedImages = new ArrayList<>();
 
         imageAdapter = new ImageAdapter(this, displayedImages);
         recyclerView.setAdapter(imageAdapter);
 
-        loadAllImages();
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        final int cacheSize = maxMemory / 8;
+        bitmapCache = new LruCache<>(cacheSize);
+
+        File dir = getFilesDir();
+        File file = new File(dir, ".NEW");
+        try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
+            directoryEntries = fileManager.readAllEntries(raf);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         imageAdapter.setOnItemClickListener(new ImageAdapter.OnItemClickListener() {
             @Override
@@ -72,7 +84,7 @@ public class ActivityImageInFile extends AppCompatActivity {
             public void onClick(View v) {
                 if (currentPage > 0) {
                     currentPage--;
-                    loadPage();
+                    loadCurrentPageImages();
                 }
             }
         });
@@ -80,42 +92,68 @@ public class ActivityImageInFile extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if ((currentPage + 1) * IMAGES_PER_PAGE < allImages.size()) {
+                if ((currentPage + 1) * IMAGES_PER_PAGE < fileManager.countFileInList(directoryEntries)) {
                     currentPage++;
-                    loadPage();
+                    loadCurrentPageImages();
                 }
             }
         });
 
-        loadPage();
+        loadCurrentPageImages();
     }
 
-    private void loadAllImages() {
-        File dir = getFilesDir();
-        File file = new File(dir, ".NEW");
-        try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
-            List<DirectoryEntry> directoryEntries = fileManager.readAllEntries(raf);
-            for (DirectoryEntry entry : directoryEntries) {
-                if (entry != null) {
-                    int pos = fileManager.byteArrayToInt(entry.getDataPos());
-                    int size = fileManager.byteArrayToInt(entry.getSize());
-                    byte[] data = fileManager.readImageFileData(raf, pos, size);
-                    Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
-                    allImages.add(bmp);
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void loadPage() {
+    private void loadCurrentPageImages() {
         displayedImages.clear();
-        int start = currentPage * IMAGES_PER_PAGE;
-        int end = Math.min(start + IMAGES_PER_PAGE, allImages.size());
-        for (int i = start; i < end; i++) {
-            displayedImages.add(allImages.get(i));
+        new LoadImagesTask().execute(currentPage);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class LoadImagesTask extends AsyncTask<Integer, Void, List<Bitmap>> {
+        @Override
+        protected List<Bitmap> doInBackground(Integer... params) {
+            int page = params[0];
+            List<Bitmap> images = new ArrayList<>();
+            File dir = getFilesDir();
+            File file = new File(dir, ".NEW");
+
+            try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
+                int count = 0;
+                int startIndex = page * IMAGES_PER_PAGE;
+                int endIndex = startIndex + IMAGES_PER_PAGE;
+
+                for (DirectoryEntry entry : directoryEntries) {
+                    if (entry != null) {
+                        if (count >= startIndex && count < endIndex) {
+                            String key = fileManager.byteArrayToString(entry.getDataPos()) + "_" + fileManager.byteArrayToString(entry.getSize());
+                            Bitmap bmp = bitmapCache.get(key);
+
+                            if (bmp == null) {
+                                int pos = fileManager.byteArrayToInt(entry.getDataPos());
+                                int size = fileManager.byteArrayToInt(entry.getSize());
+                                byte[] data = fileManager.readImageFileData(raf, pos, size);
+                                bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                bitmapCache.put(key, bmp);
+                            }
+
+                            images.add(bmp);
+                        }
+                        count++;
+                        if (count >= endIndex) {
+                            break;
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return images;
         }
-        imageAdapter.notifyDataSetChanged();
+
+        @SuppressLint("NotifyDataSetChanged")
+        @Override
+        protected void onPostExecute(List<Bitmap> bitmaps) {
+            displayedImages.addAll(bitmaps);
+            imageAdapter.notifyDataSetChanged();
+        }
     }
 }
